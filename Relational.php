e5be90f734bdde3d326283931fe4b2f19c48af73
M. Drew LaMar <mdlama@wm.edu>
2026-09-25 15:23:36 -0400

    Database: Read a column default from the variable that holds it
    getTableColumnDefault() looked up the default in $column, which is never
    assigned; the columns are in $columns. Asking any model for a column
    default raised an undefined variable and returned nothing.
    
    (cherry picked from commit f3bc9711dcf7d311b49572d91870ea9ac6dab272)

--- Relational.php
+++ Relational.php
@@ -1500,7 +1500,7 @@ class Relational implements \IteratorAggregate, \ArrayAccess
 
 		if (isset($columns[$col]))
 		{
-			return $column[$col]['default'];
+			return $columns[$col]['default'];
 		}
 
 		return null;
